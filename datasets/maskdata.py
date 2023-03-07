e955abab62d3e2712f345f2ae11ebff095bda2df
Anton <nekhai.anton@gmail.com>
2023-03-06 21:12:59 -0500

    refactored datasets

--- datasets/maskdata.py
+++ datasets/maskdata.py
@@ -8,23 +8,24 @@ from PIL import Image
 
 class MaskedPokemonDataset(Dataset):
 
-    def __init__(self, annotations_file, img_dir, transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.df = pd.read_csv(csv_file)
+        self.root_dir = root_dir
         self.transform = transform
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.df)
     
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        img_path = os.path.join(self.root_dir, self.df.iloc[idx, 0])
         # image = read_image(img_path).type(torch.float32)
         image = Image.open(img_path)
+        target = []
 
         if self.transform:
             image = self.transform(image)
 
-        return image
+        return image, target
 
 
 if __name__ == "__main__":
--- datasets/pokedata.py
+++ datasets/pokedata.py
@@ -8,12 +8,16 @@ from PIL import Image
 
 class PokemonDataset(Dataset):
 
-    def __init__(self, csv_file, root_dir, labels=None, transform=None):
+    def __init__(self, csv_file, root_dir, transform=None, 
+                 get_mask=False, get_types=False):
         self.df = pd.read_csv(csv_file)
         self.root_dir = root_dir
-        self.labels = labels
         self.transform = transform
-
+        self.get_mask = get_mask
+        self.get_types = get_types
+        self.mask_label = self.df.columns.get_loc("mask")
+        self.type_labels = [self.df.columns.get_loc(t) 
+                            for t in ["type_1", "type_2"]]
 
     def __len__(self):
         return len(self.df)
@@ -23,19 +27,21 @@ class PokemonDataset(Dataset):
         img_path = os.path.join(self.root_dir, self.df.iloc[idx, 0])
         # image = read_image(img_path).type(torch.float32)
         image = Image.open(img_path)
-
+        targets = []
         # TODO: think through how we can mesh disparate auxiliary data into one unpacking stream
         # EX: OHE types or egg_groups, open a mask image, etc. etc. 
-        if self.labels:
-            label_idxs = [self.df.columns.get_loc(label) for label in self.labels]
-            target = self.df.iloc[idx, label_idxs]
-        else:
-            target = self.df.iloc[idx, 1:]
+        if self.get_mask:
+            mask_path = os.path.join(self.root_dir, self.df.iloc[idx, self.mask_label])
+            mask = Image.open(mask_path)
+            targets.append(mask)
+        if self.get_types:
+            # need to one hot encode type labels and return the sum of the two labels we get
+            pass
 
         if self.transform:
             image = self.transform(image)
 
-        return image, target
+        return image, targets
 
 
 if __name__ == "__main__":
