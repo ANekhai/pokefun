5fc53b0216c2d834ce0d0d2eedabfa56ff281de3
Anton <nekhai.anton@gmail.com>
2023-05-12 10:59:31 -0400

    Trained a type classifier for perceptual loss

--- datasets/pokedata.py
+++ datasets/pokedata.py
@@ -10,7 +10,7 @@ class PokemonDataset(Dataset):
 
     def __init__(self, csv_file, root_dir, transform=None, 
                  get_mask=False, get_types=False):
-        self.df = pd.read_csv(csv_file)
+        self.df = pd.read_csv(csv_file).fillna("None")
         self.root_dir = root_dir
         self.transform = transform
         self.get_mask = get_mask
@@ -18,6 +18,9 @@ class PokemonDataset(Dataset):
         self.mask_label = self.df.columns.get_loc("mask")
         self.type_labels = [self.df.columns.get_loc(t) 
                             for t in ["type_1", "type_2"]]
+        
+        # type logic
+        self.types = sorted(self.df["type_2"].unique())
 
     def __len__(self):
         return len(self.df)
@@ -28,15 +31,19 @@ class PokemonDataset(Dataset):
         # image = read_image(img_path).type(torch.float32)
         image = Image.open(img_path)
         targets = []
-        # TODO: think through how we can mesh disparate auxiliary data into one unpacking stream
-        # EX: OHE types or egg_groups, open a mask image, etc. etc. 
+
         if self.get_mask:
             mask_path = os.path.join(self.root_dir, self.df.iloc[idx, self.mask_label])
             mask = Image.open(mask_path)
             targets.append(mask)
         if self.get_types:
-            # need to one hot encode type labels and return the sum of the two labels we get
-            pass
+            types = [self.df.iloc[idx, t] for t in self.type_labels]
+            type_vec = torch.zeros(len(self.types))
+            
+            for type in types:
+                type_vec[self.types.index(type)] = 1.
+
+            targets.append(type_vec)
 
         if self.transform:
             image = self.transform(image)
@@ -48,7 +55,7 @@ if __name__ == "__main__":
     img_dir = "data"
     annotations = "data/data.csv"
 
-    dset = PokemonDataset(annotations, img_dir, labels=["type_1", "type_2", "is_legendary", "is_mythical"])
+    dset = PokemonDataset(annotations, img_dir, get_types=True)
     
     print(len(dset))
 
